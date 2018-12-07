da8c35d701f5452d9873f6f4e98ed512752119ae
Fabian Krol <fabian@krol.io>
2018-12-08 00:34:48 +0100

    Point ex_ct to develop branch

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule ZmqEx.MixProject do
       {:credo, "~> 1.0.0-rc1", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.3", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:ex_ct, "~> 0.0.0"}
+      {:ex_ct, git: "https://github.com/fabiankrol/ex_ct.git", branch: "develop"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
