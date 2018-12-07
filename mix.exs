d11b2a0847527dc85f185c8a28f6b75a75050da0
Fabian Krol <fabian@krol.io>
2018-12-08 00:15:34 +0100

    Add ex_ct

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule ZmqEx.MixProject do
       {:excoveralls, "~> 0.10", only: :test},
       {:credo, "~> 1.0.0-rc1", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.3", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:ex_ct, "~> 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
